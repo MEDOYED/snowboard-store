c36c9025348cb6e7005bd0a8c7cc521995e74591
MEDOYED <mmax26072005@gmail.com>
2025-03-23 21:55:06 +0100

    feat(ButtonSignIn): active state on button sign in

--- frontend/src/shared/UI/buttons/ButtonSignIn/ButtonSignIn.jsx
+++ frontend/src/shared/UI/buttons/ButtonSignIn/ButtonSignIn.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 import { User } from "lucide-react";
 
 import "./ButtonSignIn.scss";
 
 const ButtonSignIn = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+  console.log(isActive);
+
   return (
-    <li className="button-sign-in">
+    <li className="button-sign-in" onClick={handleClick}>
       <User className="button-sign-in__icon" />
       <span>Sign in</span>
     </li>
