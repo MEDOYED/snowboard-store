b26bf8ee783f767859a91372f2dd5170cceba201
MEDOYED <mmax26072005@gmail.com>
2025-03-23 23:05:05 +0100

    layout(ModalSignIn)

---
+++ frontend/src/widgets/modals/ModalSignIn/ModalSignIn.jsx
@@ -0,0 +1,14 @@
+import "./ModalSingIn.scss";
+
+const ModalSignIn = () => {
+  return (
+    <div className="modal-sign-in">
+      <div className="modal-sign-in__content">
+        <div className="modal-sign-in__sign-in">Sign In:</div>
+        <div className="modal-sign-in__registration">Registration</div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalSignIn;
