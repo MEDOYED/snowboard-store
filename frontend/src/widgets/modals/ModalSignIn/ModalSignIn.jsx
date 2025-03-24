faa284b415aa8d57efc9487b3b8a5c99b5b97883
Roverp <yar.zubaha@proton.me>
2025-03-24 17:25:09 +0100

    layout(ModalSignIn): markup 'n' style sign-in part

--- frontend/src/widgets/modals/ModalSignIn/ModalSignIn.jsx
+++ frontend/src/widgets/modals/ModalSignIn/ModalSignIn.jsx
@@ -7,15 +7,45 @@ const ModalSignIn = () => {
   const isActive = useSelector((state) => state.signIn.isActive);
 
   const handleClick = () => {
-    dispatch(setSignInState(false));
+    //dispatch(setSignInState(false));
   };
+
   return (
     <>
       {isActive === true && (
         <div className="modal-sign-in" onClick={handleClick}>
           <div className="modal-sign-in__content">
-            <div className="modal-sign-in__sign-in">Sign In:</div>
-            <div className="modal-sign-in__registration">Registration</div>
+            <div className="modal-sign-in__sign-in">
+              <h3 className="modal-sign-in__heading">Sign in</h3>
+              <form action="#" className="modal-sign-in__form">
+                <div className="modal-sign-in__input-fields">
+                  <label>
+                    <span>Phone number or email</span>
+                    <input type="text" required />
+                  </label>
+
+                  <label>
+                    <span>Password</span>
+                    <input type="password" required />
+                  </label>
+                </div>
+
+                <div className="modal-sign-in__checkbox-row">
+                  <label className="modal-sign-in__checkbox-label">
+                    <input type="checkbox" />
+                    <span>Remember me</span>
+                  </label>
+
+                  <a href="#" className="modal-sign-in__forgot-password-link">
+                    Forgot password?
+                  </a>
+                </div>
+
+                <button className="modal-sign-in__auth-btn">Sign In</button>
+              </form>
+            </div>
+
+            <div className="modal-sign-in__sign-up">Registration</div>
           </div>
         </div>
       )}
