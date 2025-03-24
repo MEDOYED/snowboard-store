ce68ffa552aaa07eede41c34b076ae215c78c095
MEDOYED <mmax26072005@gmail.com>
2025-03-24 13:13:05 +0100

    chore(Footer): create and connect Footer and download footer-bg image

--- frontend/src/pages/PageHome/PageHome.jsx
+++ frontend/src/pages/PageHome/PageHome.jsx
@@ -1,14 +1,15 @@
-import ModalSignIn from "../../widgets/modals/ModalSignIn/ModalSignIn";
 import SectionFeaturedIn from "../../widgets/sections/SectionFeaturedIn/SectionFeaturedIn.jsx";
+import Footer from "../../widgets/layout/Footer/Footer.jsx";
+import ModalSignIn from "../../widgets/modals/ModalSignIn/ModalSignIn";
 
 import "./PageHome.scss";
 
 const PageHome = () => {
   return (
     <>
-      <div>PageHome</div>
-    
       <SectionFeaturedIn />
+      <Footer />
+
       <ModalSignIn />
     </>
   );
---
+++ frontend/src/widgets/layout/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div>
+      Footer
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
