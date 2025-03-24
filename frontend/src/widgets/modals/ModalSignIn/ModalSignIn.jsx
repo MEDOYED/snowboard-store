1a6bf21cadf4d7c87fdc3bd8041258b78059e94c
Roverp <yar.zubaha@proton.me>
2025-03-24 18:07:43 +0100

    layout(ModalSignIn): sign-up part & add close button

--- frontend/src/widgets/modals/ModalSignIn/ModalSignIn.jsx
+++ frontend/src/widgets/modals/ModalSignIn/ModalSignIn.jsx
@@ -1,3 +1,5 @@
+import { X } from "lucide-react";
+
 import { useSelector, useDispatch } from "react-redux";
 import { setSignInState } from "../../../App/store/signInSlice";
 import "./ModalSignIn.scss";
@@ -7,13 +9,13 @@ const ModalSignIn = () => {
   const isActive = useSelector((state) => state.signIn.isActive);
 
   const handleClick = () => {
-    //dispatch(setSignInState(false));
+    dispatch(setSignInState(false));
   };
 
   return (
     <>
       {isActive === true && (
-        <div className="modal-sign-in" onClick={handleClick}>
+        <div className="modal-sign-in">
           <div className="modal-sign-in__content">
             <div className="modal-sign-in__sign-in">
               <h3 className="modal-sign-in__heading">Sign in</h3>
@@ -45,7 +47,47 @@ const ModalSignIn = () => {
               </form>
             </div>
 
-            <div className="modal-sign-in__sign-up">Registration</div>
+            <div className="modal-sign-in__sign-up">
+              <div className="modal-sign-in__heading-wrapper">
+                <h3 className="modal-sign-in__heading">Create account</h3>
+                <button
+                  className="modal-sign-in__close-btn"
+                  onClick={handleClick}
+                >
+                  <X />
+                </button>
+              </div>
+
+              <form action="#" className="modal-sign-in__form">
+                <div className="modal-sign-in__input-fields">
+                  <label>
+                    <span>Name*</span>
+                    <input type="text" required />
+                  </label>
+
+                  <label>
+                    <span>Email*</span>
+                    <input type="email" required />
+                  </label>
+
+                  <label>
+                    <span>Phone number</span>
+                    <input type="tel" />
+                  </label>
+                </div>
+
+                <div className="modal-sign-in__checkbox-row">
+                  <label className="modal-sign-in__checkbox-label">
+                    <input type="checkbox" />
+                    <span>I'm agreeing with terms of service</span>
+                  </label>
+                </div>
+
+                <button className="modal-sign-in__auth-btn">
+                  Create account
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       )}
