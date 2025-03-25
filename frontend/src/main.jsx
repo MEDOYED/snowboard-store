e6f30f09e4ce7aecd807ea1f25eba1a0f845b272
Roverp <yar.zubaha@proton.me>
2025-03-25 16:17:18 +0100

    feat: add routing to PageFAQ

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,31 @@ import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { store } from "./app/store/index.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./app/App.jsx";
+import PageFAQ from "./pages/PageFAQ/PageFAQ.jsx";
 
 import "./main.scss";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>404 Error(temp)</div>,
+  },
+  {
+    path: "/FAQ",
+    element: <PageFAQ />,
+  },
+]);
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
---
+++ frontend/src/pages/PageFAQ/PageFAQ.jsx
@@ -0,0 +1,14 @@
+import Header from "../../widgets/layout/Header/Header";
+
+import "./PageFAQ.scss";
+
+const PageFAQ = () => {
+  return (
+    <>
+      <Header />
+      <div>FAQ page</div>
+    </>
+  );
+};
+
+export default PageFAQ;
