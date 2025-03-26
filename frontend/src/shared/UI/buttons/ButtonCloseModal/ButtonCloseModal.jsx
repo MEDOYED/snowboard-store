01a0e9553479e6ff9a82420794ab2d6da99f1b9c
Roverp <yar.zubaha@proton.me>
2025-03-26 17:30:33 +0100

    feat: add :focus outline for better accessibility

--- frontend/src/shared/UI/buttons/ButtonCloseModal/ButtonCloseModal.jsx
+++ frontend/src/shared/UI/buttons/ButtonCloseModal/ButtonCloseModal.jsx
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setSignInState } from "../../../../app/store/signInSlice";
 import { X } from "lucide-react";
 
