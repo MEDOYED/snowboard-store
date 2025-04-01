e18600539e4219e8cf0580956636d79a7a746488
MEDOYED <mmax26072005@gmail.com>
2025-04-01 22:38:37 +0200

    chore: rename Slider to SliderPromo

--- frontend/src/pages/PageHome/PageHomeSlider/PageHomeSlider.jsx
+++ frontend/src/pages/PageHome/PageHomeSlider/PageHomeSlider.jsx
@@ -1,15 +1,15 @@
-import Slider from "../../../widgets/sliders/Slider/Slider";
+import SliderPromo from "../../../widgets/sliders/SliderPromo/SliderPromo";
 
 import "./PageHomeSlider.scss";
 
 const PageHomeSlider = () => {
   return (
     <>
-      <Slider>
+      <SliderPromo>
         <div className="page-home-slider item-1">Item 1</div>
         <div className="page-home-slider item-2">Item 2</div>
         <div className="page-home-slider item-3">Item 3</div>
-      </Slider>
+      </SliderPromo>
     </>
   );
 };
