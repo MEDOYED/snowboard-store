6a3da24ecfeed48d641524996af21f0e63785048
MEDOYED <mmax26072005@gmail.com>
2025-04-07 23:35:01 +0200

    feat(SectionProductCategories): 1.0v of categories slider

--- frontend/src/shared/data/dataProductCategories.js
+++ frontend/src/shared/data/dataProductCategories.js
@@ -8,6 +8,17 @@ import imgCategory7 from "/img/product-categories/7.png";
 import imgCategory8 from "/img/product-categories/8.png";
 import imgCategory9 from "/img/product-categories/9.png";
 
+// temp categories
+import imgCategory10 from "/img/product-categories/1.png";
+import imgCategory11 from "/img/product-categories/2.png";
+import imgCategory12 from "/img/product-categories/3.png";
+import imgCategory13 from "/img/product-categories/4.png";
+import imgCategory14 from "/img/product-categories/5.png";
+import imgCategory15 from "/img/product-categories/6.png";
+import imgCategory16 from "/img/product-categories/7.png";
+import imgCategory17 from "/img/product-categories/8.png";
+import imgCategory18 from "/img/product-categories/9.png";
+
 const dataProductCategories = [
   {
     src: imgCategory1,
@@ -44,6 +55,42 @@ const dataProductCategories = [
   {
     src: imgCategory9,
     alt: "Gloves",
+  },
+  {
+    src: imgCategory10,
+    alt: "Snowboards",
+  },
+  {
+    src: imgCategory11,
+    alt: "Bindings",
+  },
+  {
+    src: imgCategory12,
+    alt: "Boots",
+  },
+  {
+    src: imgCategory13,
+    alt: "Sets",
+  },
+  {
+    src: imgCategory14,
+    alt: "Jackets",
+  },
+  {
+    src: imgCategory15,
+    alt: "Pants",
+  },
+  {
+    src: imgCategory16,
+    alt: "Helmets",
+  },
+  {
+    src: imgCategory18,
+    alt: "Gloves",
+  },
+  {
+    src: imgCategory17,
+    alt: "Goggles",
   },
 ];
 
