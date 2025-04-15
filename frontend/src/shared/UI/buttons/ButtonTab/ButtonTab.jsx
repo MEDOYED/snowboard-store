d9996ada4b375541ff9ecca73843a749b953c6c4
Roverp <yar.zubaha@proton.me>
2025-04-16 01:09:58 +0200

    feat(ButtonTab): use motion for animations

--- frontend/src/shared/UI/buttons/ButtonTab/ButtonTab.jsx
+++ frontend/src/shared/UI/buttons/ButtonTab/ButtonTab.jsx
@@ -1,18 +1,23 @@
 import classNames from "classnames";
+import { motion } from "motion/react";
 
 import "./ButtonTab.scss";
 
 const ButtonTab = ({ children, onClick, isActive, className }) => {
   return (
-    <li
-      className={classNames(
-        "button-tab",
-        { "button-tab--active": isActive },
-        className,
-      )}
+    <motion.li
+      className={classNames("button-tab", className)}
+      initial={{}}
+      animate={isActive ? { color: "#000" } : {}}
+      transition={{ duration: 0.15, ease: "easeInOut" }}
+      whileHover={
+        !isActive
+          ? { color: "#414647", scale: 1.1, transition: { duration: 0.1 } }
+          : {}
+      }
     >
       <button onClick={onClick}>{children}</button>
-    </li>
+    </motion.li>
   );
 };
 
