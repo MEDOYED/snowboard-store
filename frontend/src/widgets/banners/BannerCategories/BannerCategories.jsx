3bcd06c611726dda37630bd7a76d9a4c88e51d8e
MEDOYED <mmax26072005@gmail.com>
2025-04-19 16:35:43 +0200

    feat(BannerCategories): improve bunner-categories__path

--- frontend/src/widgets/banners/BannerCategories/BannerCategories.jsx
+++ frontend/src/widgets/banners/BannerCategories/BannerCategories.jsx
@@ -6,11 +6,31 @@ import "./BannerCategories.scss";
 const BannerCategories = () => {
   const location = useLocation();
   const locationPath = location.pathname;
+  console.log(locationPath);
+
+  // Split by "/", filter out empty strings
+  const locationParts = locationPath.split("/").filter(Boolean);
+
+  // Capitalize each word (make the first letter uppercase)
+  const locationCapitalized = locationParts.map(
+    (word) => word.charAt(0).toUpperCase() + word.slice(1)
+  );
+
+  const [firstWord, secondWord] = locationCapitalized;
+
   return (
     <section className="banner-categories">
       <div className="container">
         {/* потім зроблю окремий компонент CategoryPath */}
-        <p className="banner-categories__path">{locationPath}</p>
+        <p className="banner-categories__path">
+          <span>{firstWord}</span>
+          {secondWord && (
+            <>
+              <span>/</span>
+              <span>{secondWord}</span>
+            </>
+          )}
+        </p>
 
         <SectionSalesCards />
       </div>
