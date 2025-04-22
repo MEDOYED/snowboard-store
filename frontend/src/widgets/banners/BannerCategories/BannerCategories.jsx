f4002d1e3282758df4244fd34f1a7eaa72f0f284
MEDOYED <mmax26072005@gmail.com>
2025-04-22 23:13:18 +0200

    feat(BannerCategories): connect SectionBrands to BannerCategories

--- frontend/src/widgets/banners/BannerCategories/BannerCategories.jsx
+++ frontend/src/widgets/banners/BannerCategories/BannerCategories.jsx
@@ -1,15 +1,20 @@
+import { useParams } from "react-router-dom";
+
 import PathUrl from "../../../shared/UI/paths/PathUrl/PathUrl";
 import SectionSalesCards from "../../sections/SectionSalesCards/SectionSalesCards";
+import SectionBrands from "../../sections/SectionBrands/SectionBrands";
 
 import "./BannerCategories.scss";
 
 const BannerCategories = () => {
+  const { parentSlug } = useParams();
+  console.log(parentSlug);
   return (
     <section className="banner-categories">
       <div className="container">
         <PathUrl />
 
-        <SectionSalesCards numberOfCards={3} />
+        {parentSlug === "brands" ? <SectionBrands /> : <SectionSalesCards numberOfCards={3} />}
       </div>
     </section>
   );
