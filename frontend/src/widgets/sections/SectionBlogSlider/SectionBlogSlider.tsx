087738d7ea68ed221d1eb921af981fb41ac885c8
Roverp <yar.zubaha@proton.me>
2025-04-25 20:00:19 +0200

    layout(SectionBlogSlider): -> markup & style

--- frontend/src/widgets/sections/SectionBlogSlider/SectionBlogSlider.tsx
+++ frontend/src/widgets/sections/SectionBlogSlider/SectionBlogSlider.tsx
@@ -1,12 +1,66 @@
+import { useState } from "react";
+
 import { BlogSlide } from "../../../shared/types/typesBlogSlider";
 
 import "./SectionBlogSlider.scss";
 
 const SectionBlogSlider: React.FC<{ slides: BlogSlide[] }> = ({ slides }) => {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState<number>(0);
+
+  const nextSlide = (): void => {
+    setCurrentSlideIndex((prevSlideIndex) =>
+      prevSlideIndex !== slides.length - 1 ? prevSlideIndex + 1 : 0,
+    );
+  };
+
+  const prevSlide = (): void => {
+    setCurrentSlideIndex((prevSlideIndex) =>
+      prevSlideIndex !== 0 ? slides.length - 1 : prevSlideIndex - 1,
+    );
+  };
+
   return (
-    <>
-      <div>SectionBlogSlider</div>
-    </>
+    <section className="section-blog-slider">
+      <div className="section-blog-slider__slider-track slider-track">
+        {slides.map((slide) => {
+          return (
+            <article
+              className="slider-track__slide slide"
+              style={{ backgroundColor: slide.backgroundColor }}
+              key={slide.title}
+            >
+              <img
+                src={slide.mediaUrl}
+                alt={slide.mediaAlt}
+                className="slide__img"
+              />
+
+              <div className="slide_text_wrapper text-wrapper">
+                <header className="text_wrapper__blog-header blog-header">
+                  <h4 className="blog-header__section-heading">Blogs</h4>
+                  <a href="#" className="blog-header__link">
+                    All blogs
+                  </a>
+                </header>
+
+                <div className="text-wrapper__blog-body blog-body">
+                  <h2 className="blog-body__blog-title">{slide.title}</h2>
+                  <p className="blog-body__blog-description">
+                    {slide.description}
+                  </p>
+                </div>
+
+                <footer className="text-wrapper__blog-footer blog-footer">
+                  <a href="#" className="blog-footer__link">
+                    Read more
+                  </a>
+                </footer>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
