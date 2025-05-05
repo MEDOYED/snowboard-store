09915e5fe2e7791cd37d195a3834187e7cd9fc3f
MEDOYED <mmax26072005@gmail.com>
2025-05-05 16:08:21 +0200

    feat(FormSignUp): getting values of name, email, phone fields

--- frontend/src/widgets/forms/FormSignUp/FormSignUp.jsx
+++ frontend/src/widgets/forms/FormSignUp/FormSignUp.jsx
@@ -1,39 +1,57 @@
 import { useState } from "react";
 
-import InputName from "../../../shared/UI/inputs/InputName/InputName";
-
 import "./FormSignUp.scss";
 
 const FormSignUp = () => {
   const [currentValue, setCurrentValue] = useState({
     name: "",
     email: "",
+    phone: "",
   });
+
+  const onHandleChange = (e) => {
+    const changedField = e.target.name;
+    const value = e.target.value;
+
+    // more correct with destructuring the object
+    // const { name, value } = e.target;
+
+    setCurrentValue((prev) => ({
+      ...prev,
+      [changedField]: value,
+    }));
+  };
+
+  console.log(currentValue);
+
   return (
     <form action="#" className="modal-sign-in__form">
       <div className="modal-sign-in__input-fields">
         <label>
           <span>Name*</span>
-
-          <InputName
+          <input
+            name="name"
+            type="text"
             value={currentValue.name}
-            onChange={(value) =>
-              setCurrentValue((prev) => ({
-                ...prev,
-                name: value,
-              }))
-            }
+            onChange={onHandleChange}
+            required
           />
         </label>
 
         <label>
           <span>Email*</span>
-          <input type="email" required />
+          <input
+            name="email"
+            type="email"
+            value={currentValue.email}
+            onChange={onHandleChange}
+            required
+          />
         </label>
 
         <label>
           <span>Phone number</span>
-          <input type="tel" />
+          <input name="phone" type="tel" value={currentValue.phone} onChange={onHandleChange} />
         </label>
       </div>
 
