b74222cce4ebfe2701904f22ed53b3a8f2870e24
Roverp <yar.zubaha@proton.me>
2025-05-14 13:31:51 +0200

    feat: modify response products json to include all the neccesary data and excule unnessary

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -1,16 +1,52 @@
 import type { Request, Response } from "express";
+
 import pool from "../db.ts";
+import type Product from "@sharedTypes/product";
 
 const getAllProducts = async (req: Request, res: Response) => {
   try {
-    const allProducts = await pool.query(`SELECT * FROM products`);
+    const allProducts = await pool.query<Product>(`
+      SELECT
+        products.id,
+        products.name,
+        products.slug,
+        products.price,
+        products.discounted_price,
+        products.image_url,
+        products.image_alt,
+        products.description,
+        products.average_rating,
+        products.created_at,
+        products.updated_at,
+        products.targeted_at,
+        brands.name AS brand_name
+      FROM products
+      LEFT JOIN brands ON products.brand_id = brands.id;
+      `);
 
-    console.log("allProducts:", allProducts.rows);
-    res.status(200).json(allProducts.rows);
+    const products = allProducts.rows.map((product) => {
+      const price = Number(product.price);
+      const discounted_price = product.discounted_price
+        ? Number(product.discounted_price)
+        : null;
+
+      const discount: string | null = discounted_price
+        ? `-${Math.round(100 - (discounted_price / price) * 100)}%`
+        : null;
+
+      return {
+        ...product,
+        price: price,
+        discounted_price: discounted_price,
+        discount: discount,
+      };
+    });
+
+    res.status(200).json(products);
   } catch (err) {
     console.error("Database error:", err);
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
-export default getAllProducts;
+export { getAllProducts };
--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import getAllProducts from "../controllers/productController.ts";
+import { getAllProducts } from "../controllers/productController.ts";
 
 const router = express.Router();
 
