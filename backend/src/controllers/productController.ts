d107edba51bdb282f3205c756ddadd58d18f90e3
Roverp <yar.zubaha@proton.me>
2025-05-19 11:13:20 +0200

    feat(productController): add possible limit to response

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -2,10 +2,10 @@ import type { Request, Response } from "express";
 
 import pool from "../db.ts";
 import type Product from "@sharedTypes/product";
+import type { productQuery } from "src/shared/types/product";
 
 const getAllProducts = async (req: Request, res: Response) => {
-  try {
-    const allProducts = await pool.query<Product>(`
+  let sqlQuery = `
       SELECT
         products.id,
         products.name,
@@ -19,11 +19,28 @@ const getAllProducts = async (req: Request, res: Response) => {
         products.created_at,
         products.updated_at,
         products.targeted_at,
-        brands.name AS brand_name
+        brands.name AS brand_name,
+        categories.name AS category_name
       FROM products
       LEFT JOIN brands ON products.brand_id = brands.id
-      ORDER BY id DESC;
-      `);
+      LEFT JOIN categories ON products.category_id = categories.id
+      ORDER BY id DESC
+      `;
+
+  const values: unknown[] = [];
+  let placeHolderIndex = 1;
+
+  const { limit } = req.query as productQuery;
+  const parsedLimit = limit ? parseInt(limit) : undefined;
+  console.log("parsedLimit:", parsedLimit);
+
+  if (parsedLimit) {
+    sqlQuery += ` LIMIT $${placeHolderIndex}`;
+    values.push(parsedLimit);
+  }
+
+  try {
+    const allProducts = await pool.query<Product>(sqlQuery, values);
 
     const products = allProducts.rows.map((product) => {
       const price = Number(product.price);
